c84dde5a308bc926094ca99d1fa26c3dfd610ad4
makotty <hal.m.90215@gmail.com>
2021-12-23 19:01:12 +0900

    Api::V1::EpisodesControllerの作成

--- app/controllers/api/v1/episodes_controller.rb
+++ app/controllers/api/v1/episodes_controller.rb
@@ -1,2 +1,39 @@
 class Api::V1::EpisodesController < ApplicationController
+  def index
+    render json: Episode.all
+  end
+
+  def show
+    render json: Episode.find(params[:id])
+  end
+
+  def create
+    episode = Episode.new(episode_params)
+    if episode.save
+      render json: episode
+    else
+      render json: episode.errors, status: 422
+    end
+  end
+
+  def update
+    episode = Episode.find(params[:id])
+    if episode.update(episode_params)
+      render json: episode
+    else
+      render json: episode.errors, status: 422
+    end
+  end
+
+  def destroy
+    episode = Episode.find(params[:id])
+    episode.destroy
+    render json: episode
+  end
+
+  private
+
+  def episode_params
+    params.permit(:content)
+  end
 end
