44bad9d42453f524f80f8e697f1c849847fa5f34
makotty <hal.m.90215@gmail.com>
2021-12-23 19:01:24 +0900

    Api::V1::EpisodesControllerのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,14 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for 'User',
                                   at: 'auth',
                                   controllers: {
-                                    registrations: 'api/v1/auth/registrations'
+                                    registrations: 'api/v1/auth/registrations',
                                   }
 
       namespace :auth do
         resources :sessions, only: %i[index]
       end
+
+      resources :episodes
     end
   end
 end
