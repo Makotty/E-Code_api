959526b0e432c43ad201daa2bcbcf2706dddfbc7
makotty <hal.m.90215@gmail.com>
2021-12-24 15:12:21 +0900

    usersのルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      resources :episodes
+      resources :users
+
       mount_devise_token_auth_for 'User',
                                   at: 'auth',
                                   controllers: {
@@ -11,8 +14,6 @@ Rails.application.routes.draw do
       namespace :auth do
         resources :sessions, only: %i[index]
       end
-
-      resources :episodes
     end
   end
 end
