188b969f36eb4d38040003222ec09e06cfb50c16
makotty <hal.m.90215@gmail.com>
2021-12-25 01:29:06 +0900

    Episodeにカラムを追加するための変更

--- db/migrate/20211224060208_add_columns_to_episodes.rb
+++ db/migrate/20211224060208_add_columns_to_episodes.rb
@@ -1,5 +1,8 @@
 class AddColumnsToEpisodes < ActiveRecord::Migration[6.1]
   def change
+    add_column :episodes, :contributor_name, :string, after: :content
+    add_column :episodes, :contributor_image, :string, after: :contributor_name
+
     add_reference :episodes, :user, foreign_key: true, after: :content
   end
 end
